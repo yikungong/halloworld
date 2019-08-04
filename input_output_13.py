d7a14edc01fa804df3916635c815d0261bf14234
501344806@qq.com <501344806@qq.com>
2019-08-04 10:03:18 +0800

    ch13 input and output

---
+++ input_output_13.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @File  : input_output_13.py
+# @Author: Yikun Gong
+# @Date  : 2019/8/4
+# python3.7 & tensorflow 1.13.1 & pytorch1.0.1 & keras2.2.4
+
+
+def reverse(text):
+    return text[::-1]
+
+
+def is_palindrome(text):
+    return text == reverse(text)
+
+
+somthing = input('Enter tet:')
+if (is_palindrome(somthing)):
+    print("yes")
+else:
+    print("no")
